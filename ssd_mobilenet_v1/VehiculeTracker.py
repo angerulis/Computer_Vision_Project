268769fa20f6e5d0381ca3d6595e50348b46a776
Atse Ange Rulis Aké <angerulis@gmail.com>
2023-11-11 18:17:58 -0500

    pre-implement update function to update bounding box coordinates and add new vehicles if they don't match

--- ssd_mobilenet_v1/VehiculeTracker.py
+++ ssd_mobilenet_v1/VehiculeTracker.py
@@ -2,3 +2,14 @@ class VehicleTracker:
     def __init__(self):
         self.next_vehicle_id = 0
         self.vehicles = {}
+
+    def update(self, detections):
+        # detections would be a list of bounding box coordinates
+        # Implement logic to update the position of tracked vehicles
+        # and to add new vehicles if they don't match existing ones
+
+        # Example logic to assign IDs to new vehicles
+        for detection in detections:
+            if self.is_new_vehicle(detection):
+                self.vehicles[self.next_vehicle_id] = detection
+                self.next_vehicle_id += 1
